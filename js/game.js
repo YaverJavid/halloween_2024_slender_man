8373680b15c9de68e2829839439ac65aca1f2ac9
gniziemazity <gniziemazity@gmail.com>
2024-10-31 10:52:50 +0200

    Player controls

--- js/game.js
+++ js/game.js
@@ -9,10 +9,17 @@ class Game {
       };
 
       this.player = new Player(center);
+
    }
 
-   update() {
-      const { ctx, canvas } = this;
-      this.player.draw(ctx);
+   start() {
+      this.animate();
+   }
+
+   animate() {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.player.update();
+      this.player.draw(this.ctx);
+      requestAnimationFrame(() => this.animate());
    }
 }
