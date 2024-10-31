9ace3e6ddfb47a912b17e37b2f3c1e22bdc91ade
gniziemazity <gniziemazity@gmail.com>
2024-10-31 13:12:42 +0200

    3d trees

--- js/camera.js
+++ js/camera.js
@@ -1,50 +1,58 @@
 class Camera {
+   static maxViewDistance = 1000;
    constructor(center, target) {
       this.center = center;
       this.target = target;
-      this.maxViewDistance = 1000;
-      this.fovAngle = Math.PI / 2;
-      this.z = -150;
+      this.fovAngle = Math.PI * 0.5;
+      this.z = -50;
    }
 
    update() {
       this.center = this.target.center;
       const frontOffset = Vector.fromPolar(
          this.target.dir,
-         this.maxViewDistance
+         Camera.maxViewDistance
       );
       this.front = Vector.add(this.center, frontOffset);
       const leftOffset = Vector.fromPolar(
          this.target.dir - this.fovAngle / 2,
-         this.maxViewDistance
+         Camera.maxViewDistance
       );
       this.left = Vector.add(this.center, leftOffset);
       const rightOffset = Vector.fromPolar(
          this.target.dir + this.fovAngle / 2,
-         this.maxViewDistance
+         Camera.maxViewDistance
       );
       this.right = Vector.add(this.center, rightOffset);
       return [this.left, this.center, this.right];
    }
 
+   #projectPoint(p, seg, {width,height}) {
+      const p1 = projectPoint(p, seg);
+      const c = Vector.cross(
+         Vector.subtract(p1, this.center),
+         Vector.subtract(p, this.center)
+      );
+      const x = (Math.sign(c) * distance(p, p1)) / distance(this.center, p1);
+      const y = -this.z / distance(this.center, p); // can use p instead of p1
+      const cX = width / 2;
+      const cY = height / 2;
+      const scaler = Math.max(cX, cY);
+      const scaledX = cX + x * scaler;
+      const scaledY = cY + y * scaler;
+      return { x: scaledX, y: scaledY };
+   }
+
    render(ctx, points) {
+      points.sort((a, b) => distance(this.center, b) - distance(this.center, a));
+
       const seg = [this.center, this.front];
-      for (const p of points) {
-         const p1 = projectPoint(p, seg);
-         const c = Vector.cross(
-            Vector.subtract(p1, this.center),
-            Vector.subtract(p, this.center)
-         );
-         const x = (Math.sign(c) * distance(p, p1)) / distance(this.center, p1);
-         const y = -this.z / distance(this.center, p1); // can use p instead of p1
-         const cX = ctx.canvas.width / 2;
-         const cY = ctx.canvas.height / 2;
-         const scaler = Math.min(cX, cY);
-         const scaledX = cX + x * scaler;
-         const scaledY = cY + y * scaler;
-         ctx.beginPath();
-         ctx.arc(scaledX, scaledY, 5, 0, Math.PI * 2);
-         ctx.fill();
+      const projections=points.map(p=>this.#projectPoint(p, seg, ctx.canvas));
+
+      for(let i=0;i<points.length;i++){
+         const proj=projections[i];
+         const dist=distance(this.center,points[i]);
+         Tree.render(ctx, proj, dist);
       }
    }
    draw(ctx) {
